e48d187f6308a1034ba7d770dc445413e5d8096c
gkerr01 <80273316+gkerr01@users.noreply.github.com>
2023-12-07 17:49:11 +0000

    commit

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
+
 
 const app = express()
 const port = 8000
 
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json());
+app.use(cors({
+  origin: ['https://supreme-guide-x44p9gvjrqgcvgxj-8001.app.github.dev']
+}));
+app.options('/item', cors());
+app.use(express.json())
+
 
 
 items = {
@@ -33,15 +38,15 @@ app.post('/item', (req, res) => {
   // Define the required fields for creating a new item
   const requiredFields = ["user_id", "keywords", "description", "lat", "lon"]
 
-// Check if all required fields are present in the request body
+  // Check if all required fields are present in the request body
   const missingFields = requiredFields.filter(field => !(field in req.body))
 
-    // If any required field is missing, respond with a 405 status and a message
+  // If any required field is missing, respond with a 405 status and a message
   if (missingFields.length > 0) {
-    return res.status(405).json({ message: `Missing Fields: ${missingFields.join(', ')}` })
+    return res.status(405).json({ message: `Missing Fields` })
   }
 
-    // Assuming validation is successful, create a new item
+  // Assuming validation is successful, create a new item
   const newItem = {
     id: Object.keys(items).length + 1,
     user_id: req.body.user_id,
@@ -49,46 +54,53 @@ app.post('/item', (req, res) => {
     description: req.body.description || "",
     lat: req.body.lat || null,
     lon: req.body.lon || null,
-    date_from: req.body.date_from || new Date().toISOString(),
+    date_from: req.body.date_from || new Date().toISOString().replace('Z',""), // replaced  Z with an empty string as pyhton has updated to 3.12 halfway though the module 
   }
 
   items[newItem.id] = newItem
+  console.log("Posted")
+  console.log(newItem)
   res.status(201).json(newItem)
 })
 
+
 app.get('/item/:itemId', (req, res) => {
   // Extract itemId from request parameters
-  const itemId = parseInt(req.params.itemId);
+  const itemId = parseInt(req.params.itemId)
 
   // Check if the item with the specified ID exists
   if (items[itemId]) {
-    res.status(200).json(items[itemId]);
+    res.status(200).json(items[itemId])
   } else {
-    res.status(404).json({ message: 'Item not found' });
+    res.status(404).json({ message: 'Item not found' })
   }
-});
+})
+
+
+
+
 
 app.get('/items', (req, res) => {
   // Retrieve all items from the items collection
-  const allItems = Object.values(items);
+  const allItems = Object.values(items)
 
-  res.status(200).json(allItems);
-});
+  res.status(200).json(allItems)
+})
 
 app.delete('/item/:itemId', (req, res) => {
-  const itemId = parseInt(req.params.itemId);
+  const itemId = parseInt(req.params.itemId)
 
   // Check if the item with the specified ID exists
   if (items[itemId]) {
     // Delete the item
-    delete items[itemId];
+    delete items[itemId]
     // Respond with a 204 status code (No Content) for successful deletion
-    res.status(204).send();
+    res.status(204).send()
   } else {
     // Respond with a 404 status code if the item is not found
-    res.status(404).json({ message: 'Item not found' });
+    res.status(404).json({ message: 'Item not found' })
   }
-});
+})
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
