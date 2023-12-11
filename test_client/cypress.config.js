830102ad18a73f0062dd591362b69f93b1e85105
gkerr01 <80273316+gkerr01@users.noreply.github.com>
2023-12-11 11:40:01 +0000

    working

--- test_client/cypress.config.js
+++ test_client/cypress.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
     e2e: {
         supportFile: false,
         specPattern: 'cypress/**/*.cy.js',
-        videoUploadOnPasses: false,
+        video: true,
         screenshotsFolder: 'reports/screenshots',
         videosFolder: 'reports/videos',
         chromeWebSecurity: false,
